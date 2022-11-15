c9f8f65eeb292b39d34b7542b936182c6f8f1f3b
S-Movakel <S.Movakel@gmail.com>
2022-11-15 19:48:49 +0330

    push the all file to github

---
+++ assets/js/main.js
@@ -0,0 +1,79 @@
+'use strict';
+
+// Modal Variables
+const modal = document.querySelector('[data-modal]');
+const modalCloseBtn = document.querySelector('[data-modal-close]');
+const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
+
+// modal Function
+const modalCloseFunc = function () { modal.classList.add('closed') };
+
+// modal eventListener
+modalCloseOverlay.addEventListener('click', modalCloseFunc);
+modalCloseBtn.addEventListener('click', modalCloseFunc);
+
+
+// Notification Toast Variable
+const notificationToast = document.querySelector('[data-toast]');
+const toastCloseBtn = document.querySelector('[data-toast-close]');
+
+// notification toast eventlistener
+toastCloseBtn.addEventListener('click', function () {
+    notificationToast.classList.add('closed');
+})
+
+// mobile menu-variables
+const mobileMenuOpenBtn = document.querySelectorAll('[data-mobile-menu-open-btn]');
+const mobileMenuCloseBtn = document.querySelectorAll('[data-mobile-menu-close-btn]');
+const mobileMenu = document.querySelectorAll('[data-mobile-menu]');
+const overlay = document.querySelector('[data-overlay]')
+// console.log(mobileMenuOpenBtn)
+// console.log(mobileMenu)
+// console.log(mobileMenuCloseBtn)
+
+for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
+
+    // mobile menu function
+    const mobileMenuCloseFunc = function () {
+        mobileMenu[i].classList.remove('active');
+        overlay.classList.remove('active');
+    }
+
+    mobileMenuOpenBtn[i].addEventListener('click', function() {
+        mobileMenu[i].classList.add('active');
+        overlay.classList.add('active');
+    });
+
+    mobileMenuCloseBtn[i].addEventListener('click', mobileMenuCloseFunc);
+    overlay.addEventListener('click', mobileMenuCloseFunc);
+}
+
+
+// accordion variables
+const accordionBtn = document.querySelectorAll('[data-accordion-btn]');
+const accordion = document.querySelectorAll('[data-accordion]');
+// console.log(accordionBtn)
+// console.log(accordion)
+
+for (let i = 0; i < accordionBtn.length; i++) {
+
+     accordionBtn[i].addEventListener('click', function () {
+
+        const clickedBtn = this.nextElementSibling.classList.contains('active');
+
+        for (let i = 0; i < accordion.length; i++) {
+            if (clickedBtn) break;
+
+            if (accordion[i].classList.contains('active')) {
+                accordion[i].classList.remove('active');
+                accordionBtn[i].classList.remove('active');
+
+            }
+
+        }
+
+        this.nextElementSibling.classList.toggle('active');
+        this.classList.toggle('active');
+
+    });
+}
